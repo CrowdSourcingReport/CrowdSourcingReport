0b5918e92cf86e73c9df07bea8f3b0aeff975f91
Amey Kamat <ask5332@gmail.com>
2014-05-17 22:58:23 +0530

    Adding Sample Project Page

--- Content/js/NavbarLoader.js
+++ Content/js/NavbarLoader.js
@@ -1,14 +1,6 @@
 $('#navbar').load("/Content/html/navbar.html");
 
-$('document').ready(function () {
-    LoadNavbar('Home');
-});
 
-function LoadNavbar(locator){  
-	if(locator=='Home'){	
-		$('#Home').attr("class", "active");
-	}
-}   
 
 
 
