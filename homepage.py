c032f883d26295b5a0bfcb216255515123c37b01
Tanay <tanaygahlot@gmail.com>
2014-05-25 23:15:00 +0530

    Resolving path problem by deleting /html from Content/html

--- homepage.py
+++ homepage.py
@@ -5,18 +5,15 @@ import random
 import jinja2
 import os
 from google.appengine.ext.webapp import template
-#env=jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2.ext.autoescape'],autoescape=True)
 class BaseHandler(webapp2.RequestHandler):
         def render_template(self, view_filename, params = None):
             if not params:
                    params = {}
-            path = os.path.join(os.path.dirname(__file__), 'Content/html', view_filename)
+            path = os.path.join(os.path.dirname(__file__), 'Content', view_filename)
             self.response.out.write(template.render(path, params))
     
 class MainPage(BaseHandler):
 	def get(self):
-#		template=env.get_template("Content/html/frontPage.html")	
-#		self.response.write(template.render())
             self.render_template('frontPage.html')        
         
 application=webapp2.WSGIApplication([('/',MainPage)],debug=True)	
