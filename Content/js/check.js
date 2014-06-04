d608f1923a7fb85916f311ae0036174e56742e1e
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-04 16:53:59 +0530

    Update check.js
    finally...gahlot....be proud of us...:P

--- Content/js/check.js
+++ Content/js/check.js
@@ -1,23 +1,5 @@
-if(typeof(Storage)!=="undefined")
-  {
-  if (!sessionStorage.viewStatus)
-  {
-    sessionStorage.viewStatus=0;
-  }
-}
-
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    if(sessionStorage.viewStatus===0){
-      sessionStorage.viewStatus = 1;
-      document.getElementById('desktopView').style.visibility = 'visible';
-      location.reload(true);
-      console.log('Script');
-    }   
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    document.getElementById('view_meta').content = "width=1280"; 
+      
  }
  
- 
- function desktop_view(){
-  document.getElementById('view_meta').content = "width=1280";  
-  document.getElementById('desktopView').style.visibility = 'hidden';
-  location.reload(true);
-}
