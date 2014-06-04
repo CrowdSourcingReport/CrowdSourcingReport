17fc5268408f068b6d8d15bdd3080a89567c9fe9
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-04 14:27:06 +0530

    Update check.js

--- Content/js/check.js
+++ Content/js/check.js
@@ -1,9 +1,9 @@
 console.log('Script Running');
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    document.getElementById('desktop-view').style.visibilty = 'visible'
+    document.getElementById('desktop-view').style.visibility = 'visible'
  }
  function desktop_view(){
   document.getElementById('view_meta').content = "width=1024";  
-  document.getElementById('desktop-view').style.visibilty = 'hidden'
+  document.getElementById('desktop-view').style.visibility = 'hidden'
  }
  
