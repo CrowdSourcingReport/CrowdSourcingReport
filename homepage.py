e89858ca19ecf389a1f220573723fb55367bf409
Tanay <tanaygahlot@gmail.com>
2014-06-04 15:58:40 +0530

    Deleted unwanted file, Changed location of frontPage.html

--- homepage.py
+++ homepage.py
@@ -4,14 +4,14 @@ import webapp2
 import random
 import jinja2
 import os
-from google.appengine.ext.webapp import template
+
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'html')),extensions=['jinja2.ext.autoescape'], autoescape=True)
 class BaseHandler(webapp2.RequestHandler):
-        def render_template(self, view_filename, params = None):
-            if not params:
-                   params = {}
-            path = os.path.join(os.path.dirname(__file__), 'Content', view_filename)
-            self.response.out.write(template.render(path, params))
-    
+	def render_template(self, filename, params = None):
+		if not params:
+			params = {}
+		template = JINJA_ENVIRONMENT.get_template(filename )
+		self.response.write(template.render()) 
 class MainPage(BaseHandler):
 	def get(self):
             self.render_template('frontPage.html')        
