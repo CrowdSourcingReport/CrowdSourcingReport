1d120e488cf3a6d94bb56d87c412309e6b19e3f3
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-04 14:59:07 +0530

    Update check.js

--- Content/js/check.js
+++ Content/js/check.js
@@ -1,9 +1,17 @@
 console.log('Script Running');
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    document.getElementById('desktop-view').style.visibility = 'visible';
- }
- function desktop_view(){
-  document.getElementById('view_meta').content = "width=1280";  
-  document.getElementById('desktop-view').style.visibility = 'hidden';
+    var ref = document.createElement("a");
+    var vie = document.createTextNode("Desktop View");
+    ref.appendChild(vie);
+    ref.setAttribute("onClick","desktop_view()"); 
+    ref.setAttribute("href","#");
+    ref.setAttribute("id","desktop-view"); 
+    document.getElementById('desktopView').appendChild(ref);
  }
  
+ 
+ function desktop_view(){
+  document.getElementById('view_meta').content = "width=1280";  
+  var child = document.getElementById('desktop-view')
+  document.getElementById('desktopView').removeChild(child);
+ }
