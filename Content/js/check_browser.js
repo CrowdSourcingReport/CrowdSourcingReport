8e0faf6ba5135096be5ca1b368c09f26f806582c
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-04 12:07:42 +0530

    Update check_browser.js
    Browser Checking Script directly through website

--- Content/js/check_browser.js
+++ Content/js/check_browser.js
@@ -1,3 +1,5 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- alert('Mobile Browser');
+function () {
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+   alert('Mobile Browser');
+ }
 }
