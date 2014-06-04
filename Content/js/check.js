efe6e99b16cc51f07d059bd1488152017513b341
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-04 14:03:48 +0530

    Update check.js
    Force Desktop Mode On Phone directly added through website

--- Content/js/check.js
+++ Content/js/check.js
@@ -2,6 +2,7 @@ console.log('Script Running');
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
    console.log('Mobile');
    window.alert('Mobile Browser');
+   document.getElementById('view_meta').content = "width=1024";
  }
  else{
    console.log('Desktop');
