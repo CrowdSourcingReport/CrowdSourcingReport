f26a5d17210d48ac79c3fff51d5481bb0dbfe297
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-04 15:41:39 +0530

    Update check.js

--- Content/js/check.js
+++ Content/js/check.js
@@ -5,15 +5,19 @@ if(typeof(Storage)!=="undefined")
     sessionStorage.viewStatus=0;
   }
 }
-  
+
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     console.log('Script Running');
-    sessionStorage.viewStatus=1;
+    if(sessionStorage.viewStatus===0){
+      sessionStorage.viewStatus = 1;
+      document.getElementById('desktopView').style.visibility = 'visible';
+      loaction.reload(true);
+    }   
  }
  
  
  function desktop_view(){
   document.getElementById('view_meta').content = "width=1280";  
-  var child = document.getElementById('desktop-view')
-  document.getElementById('desktopView').removeChild(child);
- }
+  document.getElementById('desktopView').style.visibility = 'hidden';
+  loaction.reload(true);
+}
