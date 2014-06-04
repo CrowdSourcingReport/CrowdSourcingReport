d0061a244be0abfd1c8b61ba773c0dd8fe6658fd
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-04 13:52:48 +0530

    Update and rename check_browser.js to check.js

--- Content/js/check.js
+++ Content/js/check.js
@@ -1,8 +1,9 @@
-function () {
+console.log('Script Running');
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+   console.log('Mobile');
    window.alert('Mobile Browser');
  }
  else{
+   console.log('Desktop');
   window.alert('Desktop or anything else');
  }
-}
