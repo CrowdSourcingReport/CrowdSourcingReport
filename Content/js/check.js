651fc13e27b9612f56cc98236786995e99e865eb
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-04 14:14:40 +0530

    Update check.js

--- Content/js/check.js
+++ Content/js/check.js
@@ -1,10 +1,5 @@
 console.log('Script Running');
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-   console.log('Mobile');
-   window.alert('Mobile Browser');
-   document.getElementById('view_meta').content = "width=1024";
+  
  }
- else{
-   console.log('Desktop');
-  window.alert('Desktop or anything else');
- }
+ document.getElementById('view_meta').content = "width=1024";
