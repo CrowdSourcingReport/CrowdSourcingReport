a5de814c4327d27c06930bfecafc089bea97cf3b
CrowdSourcingReport <crowdsourcingreportteam@gmail.com>
2014-06-05 15:51:45 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -20,7 +20,7 @@ class BaseHandler(webapp2.RequestHandler):
 		params = {}
 	    #user = self.user_info
 	    #params['user'] = user
-	    path = os.path.join(os.path.dirname(__file__), 'html', 'frontPaqge.html')
+	    path = os.path.join(os.path.dirname(__file__), 'html', 'frontPage.html')
 	    self.response.out.write(template.render(path, params))
 	
 class MainPage(BaseHandler):
