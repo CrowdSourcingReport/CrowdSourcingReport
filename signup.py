79b896c5244f5472f8a50167326c18a5a98216c0
Tanay <tanaygahlot@gmail.com>
2014-06-05 14:59:29 +0530

    Keeping library __init__.py BaseHandler at backburner

--- signup.py
+++ signup.py
@@ -13,6 +13,16 @@ from google.appengine.ext import ndb
 import lib 
 from google.appengine.api import users
 
+#jinja templating environment
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'html')), extensions=['jinja2.ext.autoescape'], autoescape=True)
+
+
+#base handler that cointains all the required charteristic
+class BaseHandler(webapp2.RequestHandler):
+        def render(self,filename, parameter={}):
+                template = env.get_template(filename)
+                self.response.write(template.render(parameter))
+
 
 #data model for storing user data
 class User(ndb.Model):
@@ -21,13 +31,13 @@ class User(ndb.Model):
 	password=ndb.StringProperty(required=True)
 	email=ndb.StringProperty(required=True)
 
-class AuthenticationPage(lib.BaseHandler):
+class AuthenticationPage(BaseHandler):
 	def get(self):
 		user = users.get_current_user()
 		if not user:
 			parameter={}	
 			parameter["signinUrlGoogle"] = users.create_login_url()
-			self.render("authenticationPage.html",parameter=parameter)
+			self.render("authenticationPage.html", parameter)
 		else:
 			self.redirect("/home")
 		
