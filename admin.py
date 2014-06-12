0355aa5370d3be3e56beed7625315e0f9b849910
Tanay <tanaygahlot@gmail.com>
2014-06-12 12:42:38 +0530

    Implemented credibility check for admin console

--- admin.py
+++ admin.py
@@ -4,6 +4,7 @@
 import webapp2
 from lib import BaseHandler, NGO
 import random 
+from google.appengine.api import mail
 
 class CredibilityCheckHandler(BaseHandler):
 	def get(self):
@@ -18,10 +19,37 @@ class CredibilityCheckNGOHandler(BaseHandler):
 		parameter = {}
 		ngoQuery = NGO.query(NGO.userid == userid)	
 		ngo = ngoQuery.fetch(1)
-		parameter["ngo"] = ngo
+		parameter["ngoList"] = ngo
 		self.render("adminCredibilityCheckNGOPage.html", parameter)
-				
+	def post(self, userid):
+		descriptionAuthenticity = self.request.get("descriptionAuthenticity")			
+		eightygAuthenticity = self.request.get("eightygAuthenticity")
+		ngoQuery = NGO.query(NGO.userid == userid)
+                ngoList = ngoQuery.fetch(1)
 
+		if descriptionAuthenticity == "on" and eightygAuthenticity == "on":
+			for ngo in ngoList:	
+				mail.send_mail(sender=" <tanaygahlot@gmail.com>",
+    					          to="Tanay gahlot <tanaygahlot@gmail.com>",
+              					subject="Your NGO has been approved",
+              					body="""Dear :"""+ngo.name+"""Your csr.com account has been approved.  You can now visit
+http://www.csr.com/ and sign in using your Google Account to access new features.Please let us know if you have any questions.
+The csr.com Team
+""")				
+		else:
+			failiureReport = "Places where your ngo failed\n"
+			if descriptionAuthenticity != "on":	
+				failiureReport+=" The Description you provided isnt apt for a site like us.\n"
+			elif eightygAuthenticity != "off": 
+				failiureReport+=" Your 80G no isnt valid\n"
+			for ngo in ngoList:
+                                mail.send_mail(sender=" <tanaygahlot@gmail.com>",
+                                                  to="Tanay gahlot <tanaygahlot@gmail.com>",
+                                                subject="Your NGO has failed authentication test",
+                                                body="""Dear :"""+ ngo.name + failiureReport +"""Please let us know if you have any questions.
+The csr.com Team
+""")
+			
 class CreateFakeAccount(BaseHandler):
 	def get(self):
 		for i in range(100):
