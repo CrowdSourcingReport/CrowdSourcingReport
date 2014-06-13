830b8c90d391a20bc9e1c0282d7382c50fd1a6f5
Tanay <tanaygahlot@gmail.com>
2014-06-13 13:03:48 +0530

    implemented /admin/Authenticate

--- admin.py
+++ admin.py
@@ -58,7 +58,7 @@ class CreateFakeNGOAccount(BaseHandler):
 			ngo.credibility = False
 			ngo.eightygRegistrationNumber = str(random.randrange(1,10000))
 			ngo.description = str(random.randrange(1,100000000000000000000))
-			ngo.userid = str(random.randrange(1,1000000))
+			ngo.userid = str(random.randrange(1,10))
 			ngo.email = "tanaygahlot@gmail.com"
 			ngo.put()
 		self.response.write("Done!")
@@ -66,13 +66,13 @@ class CreateFakeNGOAccount(BaseHandler):
 class CreateFakeProject(BaseHandler):
 	def get(self):
 		for i in range(100):
-                        project = Project()
-                        project.title = str(random.randrange(20,10000))
-                        project.authenticity = False
-                        project.description = str(random.randrange(1,100000000000000000000))
-                        project.ngo = str(random.randrange(1,1000000))
-                        project.put()
-                self.response.write("Done!")
+               	        project = Project()
+                	project.title = str(random.randrange(20,10000))
+                       	project.authenticity = False
+                	project.description = str(random.randrange(1,100000000000000000000))
+                        project.ngo = str(random.randrange(1,10))
+              	        project.put()
+	    	self.response.write("Done!")
 	
 	
 
@@ -90,39 +90,36 @@ class AuthenticateHandler(BaseHandler):
 
 
 class AuthenticateProjectHandler(BaseHandler):
-        def get(self, userid):
+        def get(self, urlParameter):
+		ngo, title = urlParameter.split("_")
                 parameter = {}
-                projectQuery = Project.query(Project.userid == userid)
-                project = projectQuery.fetch(1)
-                parameter["projectList"] = ngo
-                self.render("adminCredibilityCheckNGOPage.html", parameter)
-        def post(self, userid):
+                projectQuery = Project.query(Project.ngo == ngo, Project.title == title)
+                projectList = projectQuery.fetch(1)
+                parameter["projectList"] = projectList
+                self.render("adminAuthenticateProjectPage.html", parameter)
+        def post(self, urlParameter):
+		ngoUserid, title = urlParameter.split("_")
                 descriptionAuthenticity = self.request.get("descriptionAuthenticity")
-                eightygAuthenticity = self.request.get("eightygAuthenticity")
-                ngoQuery = NGO.query(NGO.userid == userid)
-                ngoList = ngoQuery.fetch(1)
+                projectQuery = Project.query(Project.ngo == ngoUserid, Project.title == title)
+		ngoQuery = NGO.query(NGO.userid == ngoUserid)
+		ngoList = ngoQuery.fetch(1)
+                projectList = projectQuery.fetch(1)
 
-                if descriptionAuthenticity == "on" and eightygAuthenticity == "on":
-                        for ngo in ngoList:
-                                mail.send_mail(sender=" <tanaygahlot@gmail.com>",
-                                                  to= "<"+ngo.email+">",
-                                                subject="Your NGO has been approved",
-                                                body="""Dear :"""+ngo.name+"""\nYour csr.com account has been approved.  You can now visit
-http://www.csr.com/ and sign in using your Google Account to access new features.Please let us know if you have any questions.
+                if descriptionAuthenticity == "on":
+                        for project in projectList:
+				for ngo in ngoList:
+					mail.send_mail(sender=" <tanaygahlot@gmail.com>",
+                                        	          to= "<"+ngo.email+">",
+	                                            	subject="Your Project has failed authentication test",
+	                                                body="""Dear :"""+ ngo.name + failiureReport +"""Please let us know if you have any questions.
 The csr.com Team
 """)
+					self.response.write("Success Mail sent to "+ ngo.email)
                 else:
-                        failiureReport = "\nPlaces where your ngo failed\n"
-                        if descriptionAuthenticity != "on":
-                                failiureReport+=" The Description you provided isnt apt for a site like us.\n"
-                        elif eightygAuthenticity != "off":
-                                failiureReport+=" Your 80G no isnt valid\n"
+                	failiureReport = "\nPlaces where your project failed\n"
+                        failiureReport+=" The Description you provided isnt apt for a site like us.\n"
                         for ngo in ngoList:
-                                mail.send_mail(sender=" <tanaygahlot@gmail.com>",
-                                                  to= "<"+ngo.email+">",
-                                                subject="Your NGO has failed authentication test",
-                                                body="""Dear :"""+ ngo.name + failiureReport +"""Please let us know if you have any questions.
-The csr.com Team
-""")
+				for project in projectList:
+        	                       	mail.send_mail(sender=" <tanaygahlot@gmail.com>", to= "<"+ngo.email+">", subject="Your NGO has failed authentication test",body="""Dear :"""+ ngo.name + failiureReport +"""Please let us know if you have any questions. \nThe csr.com Team""")
 
-app = webapp2.WSGIApplication([('/admin/CredibilityCheck', CredibilityCheckHandler),('/admin/fake/NGO',CreateFakeNGOAccount),('/admin/fake/Project',CreateFakeProject),('/admin/CredibilityCheck/([0-9]+)', CredibilityCheckNGOHandler), ('/admin', AdminHandler ), ('/admin/Authenticate', AuthenticateHandler), ('/admin/Authenticate/([0-9]+)', AuthenticateProjectHandler)])
+app = webapp2.WSGIApplication([('/admin/CredibilityCheck', CredibilityCheckHandler),('/admin/fake/NGO',CreateFakeNGOAccount),('/admin/fake/Project',CreateFakeProject),('/admin/CredibilityCheck/([0-9]+)', CredibilityCheckNGOHandler), ('/admin', AdminHandler ), ('/admin/Authenticate', AuthenticateHandler), ('/admin/Authenticate/([0-9_a-zA-Z]+)', AuthenticateProjectHandler)])
