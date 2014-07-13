261680756451a37cceb5849e2bdf0deb123ad9a5
Amey Kamat <ask5332@gmail.com>
2014-07-13 12:05:37 +0530

    Adding Functionality to remove task

--- Content/js/addTask.js
+++ Content/js/addTask.js
@@ -3,7 +3,7 @@ var titleid, descrid, fundsid;
 function addTask(){
 	if(counter == 0){
 		var table = document.createElement('table');
-		table.setAttribute("style", "padding: 2px");
+		table.setAttribute("id", "tasktable");
 		table.innerHTML = "<thead><tr><th>Sr. No.</th><th>Title</th><th>Description</th><th>Funds</th></tr></thead><tbody></tbody>"
 		document.getElementById('tasklist').appendChild(table);
 	}
@@ -13,8 +13,22 @@ function addTask(){
 	descrid = "taskDescription" + counter;
 	fundsid= "taskFund" + counter;
 	var row = document.createElement('tr');
+	row.setAttribute("id","row"+counter.toString());
 	var string = "<td>"+counter+"<td><input type = 'text' id = " + titleid + " /></td><td><input type = 'text' " + descrid + " /></td><td><input type = 'text' " + fundsid + " /></td>"
 	row.innerHTML = string;
 	document.getElementById("count").value=countid;
 	document.getElementsByTagName('tbody')[0].appendChild(row);
+}
+
+function removeTask(){
+	if(counter > 1){
+		var element = document.getElementById("row"+counter);
+		element.parentNode.removeChild(element);
+		counter--;
+	}
+	else if(counter == 1){
+		var element = document.getElementById("tasktable");
+		element.parentNode.removeChild(element);
+		counter--;
+	}
 }
