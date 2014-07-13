10288ae4a9450e8ffb53028c1abcb84c42c6dcea
Amey Kamat <ask5332@gmail.com>
2014-07-13 10:58:23 +0530

    UI for propose

--- Content/js/addTask.js
+++ Content/js/addTask.js
@@ -3,6 +3,7 @@ var titleid, descrid, fundsid;
 function addTask(){
 	if(counter == 0){
 		var table = document.createElement('table');
+		table.setAttribute("style", "padding: 2px");
 		table.innerHTML = "<thead><tr><th>Sr. No.</th><th>Title</th><th>Description</th><th>Funds</th></tr></thead><tbody></tbody>"
 		document.getElementById('tasklist').appendChild(table);
 	}
