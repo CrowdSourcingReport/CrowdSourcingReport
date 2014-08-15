d13e692a2c4fe8f61e77cacf92fccbb1e79199f6
Vidit Shah <viditisonline@gmail.com>
2014-08-15 17:58:14 +0530

    Signup Form Changes. User Notifiers

--- Content/js/signup.js
+++ Content/js/signup.js
@@ -1,20 +1,24 @@
 function signup() {
-	Parse.initialize("3lKrL4HTUMbclk8CikblOpWZlJA7OLWkI7BMxP03", "vLhckw4U9M0TtXcPjEQLIOpnC1e8enWURTMlyoUb");
+	
+	alert("Please wait for a moment.");
 
+	Parse.initialize("3lKrL4HTUMbclk8CikblOpWZlJA7OLWkI7BMxP03", "vLhckw4U9M0TtXcPjEQLIOpnC1e8enWURTMlyoUb");
+	
 	var user = new Parse.User();
 	user.set("username",document.getElementsByName("email")[0].value)
 	user.set("first", document.getElementsByName("fname")[0].value);
 	user.set("last", document.getElementsByName("lname")[0].value);
 	user.set("email", document.getElementsByName("email")[0].value);
 	user.set("password", document.getElementsByName("pass")[0].value);
-	console.log("I am here");  
+	
 	user.signUp(null, {
 	  success: function(user) {
     alert("A verification email has been sent to you. Please verify your email.");
-    document.getElementsByName("fname")[0].innerHTML = "";
-    document.getElementsByName("lname")[0].innerHTML = "";
-    document.getElementsByName("pass")[0].innerHTML = "";
-    document.getElementsByName("pass_confirmation")[0].innerHTML = "";
+    document.getElementsByName("fname")[0].value = "";
+    document.getElementsByName("lname")[0].value = "";
+    document.getElementsByName("email")[0].value = "";
+    document.getElementsByName("pass")[0].value = "";
+    document.getElementsByName("pass_confirmation")[0].value = "";
 
   },
   error: function(user, error) {
