b45aa9f6cde0029617735e00dc47570a5ddc045d
Vidit Shah <viditisonline@gmail.com>
2014-08-15 16:49:45 +0530

    OOps

--- Content/js/signup.js
+++ Content/js/signup.js
@@ -2,10 +2,10 @@ function signup() {
 	Parse.initialize("3lKrL4HTUMbclk8CikblOpWZlJA7OLWkI7BMxP03", "vLhckw4U9M0TtXcPjEQLIOpnC1e8enWURTMlyoUb");
 
 	var user = new Parse.User();
-	user.set("first", document.getElementsByName[0]("fname").value);
-	user.set("last", document.getElementsByName[0]("lname").value);
-	user.set("email", document.getElementsByName[0]("email").value);
-	user.set("password", document.getElementsByName[0]("pass").value);
+	user.set("first", document.getElementsByName("fname")[0].value);
+	user.set("last", document.getElementsByName("lname")[0].value);
+	user.set("email", document.getElementsByName("email")[0].value);
+	user.set("password", document.getElementsByName("pass")[0].value);
 	console.log("I am here");  
 	user.signUp(null, {
 	  success: function(user) {
