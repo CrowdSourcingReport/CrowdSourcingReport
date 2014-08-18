6f706dd0a21e589918779f06ab3a4f0d3fe1ec4e
Vidit Shah <viditisonline@gmail.com>
2014-08-18 13:04:25 +0530

    LogIn

--- Content/js/signin.js
+++ Content/js/signin.js
@@ -1,5 +1,5 @@
 Parse.initialize("3lKrL4HTUMbclk8CikblOpWZlJA7OLWkI7BMxP03", "vLhckw4U9M0TtXcPjEQLIOpnC1e8enWURTMlyoUb");
-alert(sessionStorage.user + sessionStorage.pass);
+
 Parse.User.logIn(sessionStorage.user, sessionStorage.pass, {
   success: function(user) {
     // Do stuff after successful login.
